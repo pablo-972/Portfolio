60aefd35c5997dd899be806aad3a81222cdeb3b5
Pablo <135460549+pablo-972@users.noreply.github.com>
2026-05-18 02:06:34 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
 import { Link, useLocation  } from "react-router-dom";
-import { motion } from "framer-motion";
 
 const navItems = [
   { name: "Home", path: "/" },
